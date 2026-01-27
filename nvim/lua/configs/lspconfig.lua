0156be4040596f232bdf5200a701e185cdf99f62
Simonas <simonas.pacauskas@gmail.com>
2026-01-27 18:21:18 +0200

    update nvim configs

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -26,7 +26,7 @@ vim.lsp.config("rust_analyzer", {
   },
 })
 
-local servers = { "html", "cssls", "rust_analyzer", "clangd" }
+local servers = { "rust_analyzer", "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
