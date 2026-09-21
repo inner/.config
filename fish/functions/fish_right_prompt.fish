f3fcc48924a06aa9b486f80bc85bb75d74086daa
inner <simonas.pacauskas@gmail.com>
2026-09-21 09:27:25 +0300

    +upd

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,10 +1,9 @@
-function fish_right_prompt --description 'Classic shaded clock and command duration'
-    set_color 1c1c1c
-    printf '░▒▓'
-    set_color --background 1c1c1c d0d0d0
+function fish_right_prompt --description 'Command duration for slow commands'
     if set -q CMD_DURATION; and test "$CMD_DURATION" -ge 3000
-        printf '  %ss ' (math --scale=1 "$CMD_DURATION / 1000")
+        set_color 1c1c1c
+        printf '░▒▓'
+        set_color --background 1c1c1c d0d0d0
+        printf '  %ss ' (math --scale=1 "$CMD_DURATION / 1000")
+        set_color normal
     end
-    printf '  %s ' (date +%H:%M:%S)
-    set_color normal
 end
